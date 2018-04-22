5da05c30683f97a27da0076b70fe9318b4af488d
SallyTemple <pfende@gmail.com>
2018-04-22 01:04:37 -0700

    update

---
+++ app.js
@@ -0,0 +1,113 @@
+$(document).ready(function () {
+
+  
+ 
+
+  
+  
+  /*$(".navbar-brand").on("click", function (event) {
+    event.preventDefault();
+    alert("hello");
+  });
+  $("#specials").on("click", function (event) {
+    event.preventDefault();
+    alert("hello");
+  });
+  $("#hairextension").on("click", function (event) {
+    event.preventDefault();
+
+  });
+  $("#crochet").on("click", function (event) {
+    event.preventDefault();
+    alert("hello");
+  });
+ 
+  $("#brainding").on("click", function (event) {
+    event.preventDefault();
+    alert("hello");
+
+  });
+  $("#cornrows").on("click", function (event) {
+    event.preventDefault();
+    alert("hello");
+
+  });
+  $("#permandrelaxer").on("click", function (event) {
+    event.preventDefault();
+    alert("hello");
+
+  });
+  $("#styling").on("click", function (event) {
+    event.preventDefault();
+    alert("hello");
+
+  });
+  $("#haircolor").on("click", function (event) {
+    event.preventDefault();
+    alert("hello");
+
+  });
+  $("#shampoo").on("click", function (event) {
+    event.preventDefault();
+    alert("hello");
+
+  });
+  $("#flatiron").on("click", function (event) {
+    event.preventDefault();
+    alert("hello");
+
+  });
+  $("#facialwaxing").on("click", function (event) {
+    event.preventDefault();
+    alert("hello");
+
+  });
+  $("#scheduleonline").on("click", function (event) {
+    event.preventDefault();
+    alert("hello");
+
+  });
+  
+  $("#wigs").on("click", function (event) {
+    event.preventDefault();
+ 
+    $("#card-text").show();
+
+
+  });
+
+  $("#hairextensions").on("click", function (event) {
+    event.preventDefault();
+    alert("hello");
+
+  });
+
+  $("#shampooandconditioner").on("click", function (event) {
+    event.preventDefault();
+    alert("hello");
+
+  });
+
+  $("#hairaccessories").on("click", function (event) {
+    event.preventDefault();
+    alert("hello");
+
+  });
+
+  $("#africanclothing").on("click", function (event) {
+    event.preventDefault();
+    alert("hello");
+
+  });
+
+  $("#contact").on("click", function (event) {
+    event.preventDefault();
+    
+  });
+
+  $("#openinghours").on("click", function (event) {
+    event.preventDefault();
+    alert("hello");
+  });*/
+
+});
